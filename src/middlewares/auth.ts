8362485c6042663f7f5abe28c2d0bdd647542e8b
afzal <afza313dz@gmail.com>
2024-10-13 16:52:01 +0530

    changes

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,27 +1,3 @@
-// import { Request, Response, NextFunction } from 'express';
-// import jwt, { JwtPayload } from 'jsonwebtoken';
-
-// interface AuthRequest extends Request {
-//   user?: any;
-// }
-
-// export const auth = (req: AuthRequest, res: Response, next: NextFunction): void => {
-//   const token = req.header('x-auth-token');
-
-//   if (!token) {
-//     res.status(401).json({ message: 'No token provided, authorization denied' });
-//     return;
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string) as JwtPayload;
-//     req.user = decoded.user;
-//     next();
-//   } catch (error) {
-//     res.status(401).json({ message: 'Token is not valid' });
-//   }
-// };
-
 import { Request, Response, NextFunction } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
@@ -42,14 +18,13 @@ export const auth = (req: AuthRequest, res: Response, next: NextFunction): void 
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string) as JwtPayload;
-    console.log('Decoded Token:', decoded);
-    // req.user = { id: decoded.user.id, role: decoded.user.role };
+
     if (decoded && decoded.user) {
       req.user = { id: decoded.user.id, role: decoded.user.role };
       next();
     } else {
       // Handle case where decoded token does not have user info
-      // console.error('Decoded token does not contain user information:', decoded);
+      
       res.status(401).json({ message: 'Token does not contain user information' });
       return 
     }
